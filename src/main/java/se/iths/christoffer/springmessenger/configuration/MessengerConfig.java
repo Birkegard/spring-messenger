11a93ddf210141d78ceb30399229969f9a07221b
Birkegard <birkegard@hotmail.se>
2026-04-10 10:39:45 +0200

    Added configuration

--- src/main/java/se/iths/christoffer/springmessenger/configuration/MessengerConfig.java
+++ src/main/java/se/iths/christoffer/springmessenger/configuration/MessengerConfig.java
@@ -3,12 +3,21 @@ package se.iths.christoffer.springmessenger.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import se.iths.christoffer.springmessenger.messaging.EmailSender;
+import se.iths.christoffer.springmessenger.messaging.Messenger;
+import se.iths.christoffer.springmessenger.service.MessageService;
+
+import java.util.Map;
 
 @Configuration
 public class MessengerConfig {
 
-    @Bean
+    @Bean("email")
     public EmailSender emailSender() {
         return new EmailSender();
+    }
+
+    @Bean
+    public MessageService messageService(Map<String, Messenger> messengers) {
+        return new MessageService(messengers);
     }
 }
--- src/main/java/se/iths/christoffer/springmessenger/service/MessageService.java
+++ src/main/java/se/iths/christoffer/springmessenger/service/MessageService.java
@@ -1,28 +1,21 @@
 package se.iths.christoffer.springmessenger.service;
 
-import org.springframework.stereotype.Service;
-import se.iths.christoffer.mytextformatter.TextFormatter;
 import se.iths.christoffer.springmessenger.messaging.Messenger;
 import se.iths.christoffer.springmessenger.model.Message;
 
 import java.util.Map;
 
-@Service
 public class MessageService {
     private final Map<String, Messenger> messengers;
 
-    private final TextFormatter textFormatter;
-
-    public MessageService(Map<String, Messenger> messengers, TextFormatter textFormatter) {
+    public MessageService(Map<String, Messenger> messengers) {
         this.messengers = messengers;
-        this.textFormatter = textFormatter;
     }
 
     public void send(Message message) {
         for (String type : messengers.keySet()) {
             if (message.getType().equals(type)) {
                 Messenger messenger = messengers.get(type);
-                message.setMessage(textFormatter.toUpperCase(message.getMessage()));
                 messenger.send(message);
                 break;
             }
